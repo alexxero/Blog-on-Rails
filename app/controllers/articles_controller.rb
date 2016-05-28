86c748ef0b0617daa8dbfb3a157f0e04e3a31138
alexxero <alexxero@mail.ru>
2016-05-28 14:12:14 +0300

    Article#edit and articles#update implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,20 @@ class ArticlesController < ApplicationController
     @articles = Article.order(id: :desc)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
