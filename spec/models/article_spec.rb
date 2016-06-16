b976e6808c5e12750cc0d9414aee588b34709ea1
alexxero <alexxero@mail.ru>
2016-06-16 14:16:35 +0300

    testing subject method

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Article do
-  
+
   describe 'validations' do
     it {should validate_presence_of :title}
     it {should validate_presence_of :text}
@@ -9,6 +9,15 @@ describe Article do
 
   describe 'associations' do
     it {should have_many :comments}
+  end
+
+  describe '#subject' do
+    it 'return the articles title' do
+      #создаём объект article
+      article = create(:article, title: 'Lorem Ipsum')
+      #проверяем (assert) метод subject созданный в Article.rb
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
   end
 
 end
