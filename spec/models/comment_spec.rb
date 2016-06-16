8f04c353c5bb01bba8e3cfe8fee20ffe0d2bcfe8
alexxero <alexxero@mail.ru>
2016-06-16 15:21:22 +0300

    Length of comment body spec added

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Comment do
 
-  it {should belong_to :article}
+  describe 'associations' do
+    it {should belong_to :article}
+  end
+
+  describe 'validations' do
+    it {should validate_length_of :body}
+  end
 
 end
