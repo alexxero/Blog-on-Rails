a365567ca280f3d812a60b818ad48259ce90c389
alexxero <alexxero@mail.ru>
2016-06-19 11:20:15 +0300

    Moving auto_sign_up method to support/session_helper.rb

--- spec/features/vistior_create_account_spec.rb
+++ spec/features/vistior_create_account_spec.rb
@@ -3,13 +3,7 @@ require 'spec_helper'
 feature 'Account Creation' do
 
   scenario 'allows a guest to create account' do
-    visit new_user_registration_path
-    fill_in :user_username, :with => 'UserAccount'
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_password, :with => '123456'
-    fill_in :user_password_confirmation, :with => '123456'
-    click_button 'Sign up'
-
+    auto_sign_in
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 
--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,10 +1,8 @@
-def sign_up
-  #visit new_user_registration_path
-
-  fill_in :user_username, :with => 'UserBot'
-  fill_in :user_email, :with => 'example@mail.com'
-  fill_in :user_password, :with => '1234567'
-  fill_in :user_password_confirmation, :with => '1234567'
-
+def auto_sign_in
+  visit new_user_registration_path
+  fill_in :user_username, :with => 'UserAccount'
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_password, :with => '123456'
+  fill_in :user_password_confirmation, :with => '123456'
   click_button 'Sign up'
 end
