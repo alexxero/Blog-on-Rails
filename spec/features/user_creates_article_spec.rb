afda764cc7b9aeb23cf65e4a75f75a894bc6de18
alexxero <alexxero@mail.ru>
2016-06-20 14:32:33 +0300

    New spec: user add new article

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 feature 'Article Creation' do
-  before(:all) do
+  before(:each) do
     auto_sign_in
   end
 
   scenario 'allows user to visit new_article page' do
     visit new_article_path
     expect(page).to have_content I18n.t('articles.new_article')
+  end
+
+  scenario 'allows user to add new article' do
+    visit new_article_path
+    fill_in :article_title, :with => 'New Article title'
+    fill_in :article_text, :with => 'New Article text'
+    click_button 'Submit'
+    #expect(page).to have_content I18n.t('articles.new_article')
   end
 
 end
