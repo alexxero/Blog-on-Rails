ba687f6832fe4fc9dc269714a1a8b24d21d8e667
alexxero <alexxero@mail.ru>
2016-06-20 14:57:34 +0300

    Moving 'user add comments' spec to separate file

--- spec/features/user_adds_comment_spec.rb
+++ spec/features/user_adds_comment_spec.rb
@@ -5,17 +5,15 @@ feature 'Article Creation' do
     auto_sign_in
   end
 
-  scenario 'allows user to visit new_article page' do
-    visit new_article_path
-    expect(page).to have_content I18n.t('articles.new_article')
-  end
-
   scenario 'allows user to add new article' do
     visit new_article_path
     fill_in :article_title, :with => 'New Article title'
     fill_in :article_text, :with => 'New Article text'
     click_button 'Submit'
-    expect(page).to have_content 'New Article title'
-  end
 
+    visit '/articles/1'
+    fill_in :comment_body, :with => 'TestComment'
+    click_button 'Submit comment'
+    expect(page).to have_content 'TestComment'
+  end
 end
