7d9a7cb39ad39c38892397a9a515893db82c9142
alexxero <alexxero@mail.ru>
2016-06-20 14:53:54 +0300

    New spec: 'user adds comment to an article'

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -18,4 +18,15 @@ feature 'Article Creation' do
     expect(page).to have_content 'New Article title'
   end
 
+  scenario 'allows user to add new article' do
+    visit new_article_path
+    fill_in :article_title, :with => 'New Article title'
+    fill_in :article_text, :with => 'New Article text'
+    click_button 'Submit'
+    
+    visit '/articles/1'
+    fill_in :comment_body, :with => 'TestComment'
+    click_button 'Submit comment'
+    expect(page).to have_content 'TestComment'
+  end
 end
