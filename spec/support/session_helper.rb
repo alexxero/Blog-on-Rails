26bebdf2f594fb8a21002debfd9a9d348563693d
alexxero <alexxero@mail.ru>
2016-06-21 13:58:35 +0300

    session_helper user data updates

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,7 +1,7 @@
 def auto_sign_in
   visit new_user_registration_path
-  fill_in :user_username, :with => 'UserAccount'
-  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'UserAccount12'
+  fill_in :user_email, :with => 'user12@example.com'
   fill_in :user_password, :with => '123456'
   fill_in :user_password_confirmation, :with => '123456'
   click_button 'Sign up'
