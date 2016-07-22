6c6e48e985ac5b4c0763aba4d8892ced60513d64
alexxero <alexxero@mail.ru>
2016-07-22 22:20:06 +0300

    Misspelling correction and avatar sizing

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :comments
 
   # Set sizing for images
-  has_attached_file :avatar, styles: { small: '64x64', med: '100x100', large: '200x200'}, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { small: '40x40', med: '100x100', large: '200x200'}, :default_url => '/images/:style/missing.png'
   # Validate content type
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   # Validate filename
